a6c311830ebb6d33d7719f4c57ceb54db2a0cd4c
Yogendra Mer <yogendra@sav.com>
2023-04-20 13:46:53 +0530

    Test

--- front_end/main.py
+++ front_end/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="app/templates")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+   return templates.TemplateResponse("hello.html", {"request": request})
+
+
+# @app.get("/hello", response_class=HTMLResponse)
+# async def hello(request: Request):
+#    return templates.TemplateResponse("hello.html", {"request": request})
